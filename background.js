62d6442e45184c426003ceaf81a4e0bee8a3037a
Halil Kahraman <52932792+hibrhmk@users.noreply.github.com>
2020-06-10 17:24:47 +0300

    upgrade

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 // background.js
-chrome.browserAction.onClicked.addListener(
-    function(tab) {
-        chrome.tabs.executeScript(tab.id, {
-            "file": "content.js"
-        });
-    });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        if (tab.url.indexOf("open.spotify.com") != -1) {
+            chrome.tabs.executeScript({file: "content.js"}, function(result) {});
+        }
+    }
+});
--- content.js
+++ content.js
@@ -1,4 +1,28 @@
-alert(document.querySelectorAll('.react-contextmenu-wrapper'))
-document.addEventListener('DOMContentLoaded', function() {
-    console.log(document.querySelectorAll('.react-contextmenu-wrapper'));
-}, false);
+
+function setButtons() {
+    setTimeout(function() {
+        let tracks = document.querySelectorAll('.tracklist div.react-contextmenu-wrapper .tracklist-col.name .track-name-wrapper')
+        let tracksList = [];
+        for(let i = 0;i<tracks.length;i++){
+            let artistname = "";
+            let songname = "";
+            if(tracks[i].querySelector('.tracklist-name') !== undefined){
+                songname = tracks[i].querySelector('.tracklist-name').innerText;
+            }
+            if(tracks[i].querySelector('.tracklist-row__artist-name-link') !== undefined){
+                artistname = tracks[i].querySelector('.tracklist-row__artist-name-link').innerText;
+            }
+
+            tracksList.push({'name':songname,'artist':artistname})
+        }
+        let elms = document.querySelectorAll('.tracklist div.react-contextmenu-wrapper .tracklist-col.more .tracklist-top-align')
+        for(let i = 0;i<elms.length;i++){
+            if(elms[i].childNodes.length === 1){
+                let button = `<a href="https://www.youtube.com/results?search_query=`+tracksList[i].artist+` `+tracksList[i].name+`" target="_blank" style="text-decoration: none;color: #fff; position: absolute;top: 17px;right: 160px !important;">YT</a>`;
+                elms[i].insertAdjacentHTML('afterbegin', button);
+            }
+        }
+    }, 2000);
+}
+
+setButtons();
